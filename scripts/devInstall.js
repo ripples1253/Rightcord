ee4e1a5ba544e40ab8b6b2bdb8d2a539be19eebb
JeanOUINA <jeanouina@lightcord.org>
2021-04-29 16:21:11 +0200

    remove cross-spawn

--- scripts/devInstall.js
+++ scripts/devInstall.js
@@ -3,14 +3,12 @@ const { join } = require("path")
 const PROJECT_DIRNAME=join(__dirname, "..")
 const child_process = require("child_process")
 
-let env = process.env
-
 function spawnProcess(cmd, ...args) {
     const postfix = cmd === "node" ? ".exe" : ".cmd"
     cmd = process.platform === "win32" ? cmd + postfix : cmd
     let result = child_process.spawnSync(cmd, args, {
         cwd: PROJECT_DIRNAME,
-        env: env,
+        env: process.env,
         stdio: "inherit"
     })
     if (result.error){
@@ -21,18 +19,12 @@ function spawnProcess(cmd, ...args) {
     }
 
 }
-let result;
 spawnProcess("npm", "rm", "electron")
 spawnProcess("npm", "i")
-if (process.platform === "win32"){
-    result = spawnProcess("npm", "i", "--save-dev", "--arch=ia32", "electron@9")
-} else {
-    result = spawnProcess("npm", "i", "--save-dev", "--arch=x64", "electron@9")
-}
-
-let runString = "`npm test`"
+const arch = process.platform === "win32" ? "ia32" : "x64"
+spawnProcess("npm", "i", "--save-dev", "--arch="+arch, "electron@9")
 
 spawnProcess("node", join(PROJECT_DIRNAME, "scripts", "installSubModules.js"))
 
 
-console.log("Everything is installed. You should be able to do "+runString+" to compile everything and launch.")
+console.log("Everything is installed. You should be able to do `npm test` to compile everything and launch.")
